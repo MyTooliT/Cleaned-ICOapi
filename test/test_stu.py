d5399947788a933b9624ac0896acc6113205a98d
René Schwaiger <sanssecours@me.com>
2025-07-28 13:12:31 +0200

    Test: Add comment banners

--- test/test_stu.py
+++ test/test_stu.py
@@ -1,11 +1,17 @@
+# -- Imports ------------------------------------------------------------------
+
 from fastapi.testclient import TestClient
 
 from icoapi.api import app
 
+# -- Globals ------------------------------------------------------------------
+
 client = TestClient(app)
 stu_prefix = "/api/v1/stu"
 
-def test_root():
+# -- Tests --------------------------------------------------------------------
+
+def test_root() -> None:
     """Test endpoint ``/``"""
     
     response = client.get(stu_prefix)
