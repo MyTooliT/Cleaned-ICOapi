4d98084b01e7c0d1a4bb366bed893fa51e6872d0
René Schwaiger <sanssecours@me.com>
2025-08-07 15:37:57 +0200

    Test: Add basic test for measurement start

--- test/test_measurement.py
+++ test/test_measurement.py
@@ -1,5 +1,7 @@
 # -- Imports ------------------------------------------------------------------
 
+from asyncio import sleep
+
 from pytest import mark
 
 # -- Classes ------------------------------------------------------------------
@@ -25,12 +27,58 @@ class TestSTU:
         ):
             assert key in body
 
-    async def test_start(self, measurement_prefix, client) -> None:
+    async def test_start(self, connect, measurement_prefix, client) -> None:
         """Test endpoint ``/start``"""
+
+        node = connect
+
+        start = str(measurement_prefix / "start")
+        stop = str(measurement_prefix / "stop")
+
+        # ========================
+        # = Test Normal Response =
+        # ========================
+
+        response = await client.post(
+            start,
+            json={
+                "name": node["name"],
+                "mac": node["mac_address"],
+                "time": 0,
+                "first": {
+                    "channel_number": 1,
+                    "sensor_id": "Acceleration Sensor",
+                },
+                "second": {
+                    "channel_number": 0,
+                    "sensor_id": "",
+                },
+                "third": {
+                    "channel_number": 0,
+                    "sensor_id": "",
+                },
+                "ift_requested": False,
+                "ift_channel": "",
+                "ift_window_width": 0,
+                "adc": {
+                    "prescaler": 2,
+                    "acquisition_time": 8,
+                    "oversampling_rate": 64,
+                    "reference_voltage": 3.3,
+                },
+                "meta": {"version": "", "profile": "", "parameters": {}},
+            },
+        )
+        assert response.status_code == 200
+
+        # Wait for measurement to take place
+        await sleep(10)
+
+        response = await client.post(stop)
 
         # =======================
         # = Test Error Response =
         # =======================
 
-        response = await client.post(str(measurement_prefix / "start"))
+        response = await client.post(start)
         assert response.status_code == 422
