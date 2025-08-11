b18558248e87e8ffd7af96981a92c742ce1f3325
René Schwaiger <sanssecours@me.com>
2025-08-11 15:47:34 +0200

    Test: Extend test for state endpoint

--- test/test_general.py
+++ test/test_general.py
@@ -14,5 +14,15 @@ class TestGeneral:
         response = await client.get(str(state_prefix))
 
         assert response.status_code == 200
+
         body = response.json()
         assert body["can_ready"] is True
+
+        disk_capacity = body["disk_capacity"]
+        assert disk_capacity["total"] >= disk_capacity["available"]
+        assert disk_capacity["available"] > 0
+
+        measurement_status = body["measurement_status"]
+        for attribute in ("instructions", "name", "start_time", "tool_name"):
+            assert measurement_status[attribute] is None
+        assert measurement_status["running"] is False
