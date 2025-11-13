22f5f1d4181f6f140f250de9de3884308812bef6
René Schwaiger <sanssecours@me.com>
2025-11-13 11:32:39 +0100

    STH Scripts: Fix Pylint warnings

--- icoapi/scripts/stu_scripts.py
+++ icoapi/scripts/stu_scripts.py
@@ -1,9 +1,13 @@
+"""Scripts for stationary transceiver unit (STU)"""
+
 from icostate import ICOsystem
 from icotronic.can.error import NoResponseError
 from icoapi.models.models import STUDeviceResponseModel
 
 
 async def get_stu(system: ICOsystem) -> list[STUDeviceResponseModel]:
+    """Get connected STU"""
+
     mac_eui = await system.stu.get_mac_address()
     dev = STUDeviceResponseModel(device_number=1, mac_address=mac_eui.format(), name="STU 1")
 
@@ -11,6 +15,8 @@ async def get_stu(system: ICOsystem) -> list[STUDeviceResponseModel]:
 
 
 async def reset_stu(system: ICOsystem) -> bool:
+    """Reset STU"""
+
     try:
         await system.reset_stu()
         return True
